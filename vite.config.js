63f17c0730cf7f53f97b81d7701c17cece29029f
Hassan198646 <hasan.niazi333@gmail.com>
2025-04-11 01:55:27 +0500

    working on design

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@assets': '/src/assets', // Adjust the path accordingly
-    },
-  },
+  
 })
